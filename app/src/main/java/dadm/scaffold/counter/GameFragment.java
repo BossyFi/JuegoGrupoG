cf960fcd2e018d776249ecabe5548e47001c42b8
Elnidas <pedroydani1@hotmail.com>
2021-11-26 19:54:57 +0100

    Actualizado el parallax

--- app/src/main/java/dadm/scaffold/counter/GameFragment.java
+++ app/src/main/java/dadm/scaffold/counter/GameFragment.java
@@ -122,7 +122,10 @@ public class GameFragment extends BaseFragment implements View.OnClickListener, 
         theGameEngine.setTheInputController(new JoystickInputController(getView()));
         scoreGameObject = new ScoreGameObject(getView(), R.id.score_value);
         theGameEngine.addGameObject(scoreGameObject);
-        theGameEngine.addGameObject(new ParallaxBackground(theGameEngine, 20, R.drawable.seamless_space_0));
+        theGameEngine.addGameObject(new ParallaxBackground(theGameEngine, 50, R.drawable.seamless_space_0));
+
+        theGameEngine.addGameObject(new ParallaxBackground(theGameEngine, 120, R.drawable.p_cuatro));
+
         //theGameEngine.addGameObject(new SpaceShipPlayer(theGameEngine));
         theGameEngine.addGameObject(new LivesCounter(getView(), R.id.lives_value));
         theGameEngine.addGameObject(new FramesPerSecondCounter(theGameEngine));
