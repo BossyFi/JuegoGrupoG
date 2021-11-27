07e8daa8175746c52737a08bd2e983dd000907ea
Bozhidar Petrov Valchev <acbossy@gmail.com>
2021-11-27 10:05:53 +0100

    - Nueva música para el juego

--- app/src/main/java/dadm/scaffold/sound/SoundManager.java
+++ app/src/main/java/dadm/scaffold/sound/SoundManager.java
@@ -67,7 +67,7 @@ public final class SoundManager {
         try {
             // Important to not reuse it. It can be on a strange state
             bgPlayer = new MediaPlayer();
-            AssetFileDescriptor afd = context.getAssets().openFd("sfx/Riccardo_Colombo_-_11_-_Something_mental.mp3");
+            AssetFileDescriptor afd = context.getAssets().openFd("sfx/musica_menu.mp3");
             bgPlayer.setDataSource(afd.getFileDescriptor(),
                     afd.getStartOffset(), afd.getLength());
             bgPlayer.setLooping(true);
